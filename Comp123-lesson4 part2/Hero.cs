a5a4f867107a2acdbb8fdc71142cea0d5c2e975d
Rishya <sithiravel3@gmail.com>
2015-01-27 13:41:43 -0500

    Addedd hero class to program

---
+++ Comp123-lesson4 part2/Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_lesson4_part2
+{
+    class Hero
+    {
+
+        //PRIVATE PROPERTIES +++++++++++++++++++
+        bool bravery = true;
+        private int health;
+        private int smarts;
+        private string gender;
+
+        //CONSTRUCTOR +++++++++++++++++
+        public Hero(int health, int smarts, string gender)
+        {
+            this.health = health;
+            this.smarts = smarts;
+            this.gender = gender;
+        }
+        //PUBLIC METHODS
+        public void doesGoodThings()
+        {
+            Console.WriteLine("Doin' good things ...yeah!");
+        }
+    }
+}
---
+++ Comp123-lesson4 part2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_lesson4_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero myHero = new Hero(7,8,"female");
+            myHero.doesGoodThings();
+            //wait for input before closing console
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
